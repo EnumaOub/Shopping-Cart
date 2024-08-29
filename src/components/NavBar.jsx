19dfd772ccdaacfaf727a00091f933e1a088c027
EnumaOub <rayane.oubenali@gmail.com>
2024-08-29 22:23:11 +0200

    Added style components to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,55 @@
-import {
-    NavLink,
-  } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 export function NavBar() {
     return (
-        <nav id="navBar">
-            <li>
-                <NavLink to="/"
-                 activeClassName="active" >
-                    Home
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/shop"
-                 activeClassName="active" >
-                    Shop
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/cart"
-                 activeClassName="active" >
-                    Cart
-                </NavLink>
-            </li>  
-        </nav>
+        <StyledNav>
+      <Logo>Mystic Emporium</Logo>
+      <NavLinks>
+        <StyledNavLink to="/"
+                 activeClassName="active" >Home</StyledNavLink>
+        <StyledNavLink to="/shop"
+                 activeClassName="active" >Shop</StyledNavLink>
+        <StyledNavLink to="/cart"
+                 activeClassName="active" >Cart</StyledNavLink>
+      </NavLinks>
+    </StyledNav>
+        
     );
 }
+
+const StyledNav = styled.nav`
+  background-color: #333;
+  padding: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  color: #fff;
+  font-family: 'MedievalSharp', cursive;
+  font-size: 1.8rem;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  color: #fff;
+  text-decoration: none;
+  font-size: 1.2rem;
+  transition: color 0.3s;
+
+  &.active {
+    color: #f39c12;
+    font-weight: bold;
+  }
+
+  &:hover {
+    color: #f39c12;
+  }
+`;
