b02b33b6eddf92065e110ef47f9c3a5239aa9e75
Ray <rayane.oubenali@gmail.com>
2024-09-08 08:01:47 +0200

    Deleted test cart call in Cart routes

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -9,13 +9,8 @@ import { CartContext } from "../components/CartProvider";
 export function Cart() {
     console.log("Cart")
 
-    const { cart, addToCart } = useContext(CartContext);
-    useEffect(() => {
-        addToCart("Test Cart in Site", 45613, 214)
-        addToCart("Test Cart in Site", 4111, 25)
-        addToCart("Test 22 Cart in Site", 145, 25)
-    }, []);
-        console.log(cart)
+    const { cart } = useContext(CartContext);
+
     return (
         <div id="cart">
             <Header 
